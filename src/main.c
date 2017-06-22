c014f6fcf31d9fedc002040484dbe72a942be218
Thiago Silva <thiagosilva@trixlog.com>
2017-06-22 15:17:57 -0300

    Add USART library and implement a simple application test for send a message by the driver.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,53 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
+  * @author  thiagosilva-trix
+  * @version V1.0.0
+  * @date    22 de jun de 2017
+  * @brief   The main program body.
+  ******************************************************************************   
+  */
 
-
+/* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx.h"
-			
+#include "usart.h"
 
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  A simple delay generate function.
+  * @param  None
+  * @retval None
+  */
+void Delay(int counter)
+{
+  int i;
+
+  for(i = 0; i < counter*1000; i++);
+}
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
 int main(void)
 {
+	/* Initialize USART1 with default params:
+		8b,1 stop-bit,none parity, no flow-control,115200
+	*/
+	USART1_DeInit();
 
-	for(;;);
+	while(1)
+	{
+		USART1_PutString("Hello USART!\n");
+		Delay(500);
+	}
 }
+
+/*******************************END OF FILE************************************/
